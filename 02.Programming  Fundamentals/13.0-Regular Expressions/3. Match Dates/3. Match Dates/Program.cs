023b62543dfe0046edb691055723ad59617b3015
Viktor Vazov <vazov5@yahoo.com>
2020-12-08 23:24:28 +0200

    add dates exercise

--- 02.Programming  Fundamentals/13.0-Regular Expressions/3. Match Dates/3. Match Dates/Program.cs
+++ 02.Programming  Fundamentals/13.0-Regular Expressions/3. Match Dates/3. Match Dates/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
+
 
 namespace _3._Match_Dates
 {
@@ -6,7 +9,23 @@ namespace _3._Match_Dates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string pattern = @"\b(?<day>\d{2})([-.\/])(?<mounth>[A-Z]\w{2})\1(?<year>\d{4})";
+            Regex regex = new Regex(pattern);
+
+            string input = Console.ReadLine();
+
+            var matches = regex.Matches(input);
+           
+
+            foreach (Match date in matches)
+            {
+                var day = date.Groups["day"];
+                var mounth = date.Groups["mounth"];
+                var year = date.Groups["year"];
+
+                Console.WriteLine($"Day: {day}, Month: {mounth}, Year: {year}");
+            }
+
         }
     }
 }

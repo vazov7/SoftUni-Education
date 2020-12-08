a02ac0af4e87e1902a1f040e40a9cbffa5d83cc2
Viktor Vazov <vazov5@yahoo.com>
2020-12-08 21:42:51 +0200

    add exercise

---
+++ 02.Programming  Fundamentals/13.0-Regular Expressions/2. Match Phone Number/2. Match Phone Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace _2._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"[+]359([ |-])2(\1)222(\1)2222\b";
+
+            Regex regex = new Regex(pattern);
+
+            string input = Console.ReadLine();
+
+            var matches = regex.Matches(input);
+
+
+            var matchedPhones = matches.Select(a => a.Value.Trim());
+
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}

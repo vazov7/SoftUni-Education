e9d86a9582dbf29cce5e572aaf1c4a3a09f40184
Viktor Vazov <vazov5@yahoo.com>
2020-12-09 22:43:02 +0200

    add changes

---
+++ 02.Programming  Fundamentals/15.0-Basic Web Project/01.Phonebook/Controllers/ProneBookController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _01.Phonebook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01.Phonebook.Controllers
+{
+    public class ProneBookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(DataContext.Contacts);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                DataContext.Contacts.Add(model);
+            }
+            else
+            {
+                TempData["Error"] = "Невалидни данни";
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}

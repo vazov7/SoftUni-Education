8095dca484a684cf19a46295e04e3a3fd7905649
Viktor Vazov <vazov5@yahoo.com>
2020-12-09 22:39:52 +0200

    add changes

---
+++ 02.Programming  Fundamentals/15.0-Basic Web Project/01.Phonebook/01.Phonebook/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _01.Phonebook.Models
+{
+    public static class DataContext
+    {
+        public static List<Contact> Contacts { get; set; }
+    }
+}

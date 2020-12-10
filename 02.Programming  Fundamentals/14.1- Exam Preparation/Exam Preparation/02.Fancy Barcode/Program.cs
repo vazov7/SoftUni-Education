956a789eb8441ade20d74c64d8fab85da2c1cec1
Viktor Vazov <vazov5@yahoo.com>
2020-12-11 00:31:10 +0200

    add exercise and change .gitignore

--- 02.Programming  Fundamentals/14.1- Exam Preparation/Exam Preparation/02.Fancy Barcode/Program.cs
+++ 02.Programming  Fundamentals/14.1- Exam Preparation/Exam Preparation/02.Fancy Barcode/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace _02.Fancy_Barcode
 {
@@ -6,7 +8,43 @@ namespace _02.Fancy_Barcode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string pattern = @"(@\#+\b)(?<body>[A-Z][A-Za-z0-9]{4,}[A-Z])(@#+)";
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                string input = Console.ReadLine();
+
+                Match match = Regex.Match(input, pattern);
+                if (match.Success)
+                {
+                    string body = match.Value;
+
+                    string temp = string.Empty;
+
+                    for (int j = 0; j < body.Length; j++)
+                    {
+                        if (Char.IsDigit(body[j]))
+                        {
+                            temp += body[j];
+                        }
+                    }
+
+                    if (temp == "")
+                    {
+                        Console.WriteLine($"Product group: 00");
+                    }
+                    else
+                    {
+
+                        Console.WriteLine($"Product group: {temp}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
         }
     }
 }

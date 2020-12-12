18b346920554786097291a05faa5007acc1e0e3a
Viktor Vazov <vazov5@yahoo.com>
2020-12-12 23:27:58 +0200

    change emoji detector

--- 02.Programming  Fundamentals/14.0 Exam Preparation/Exam Preparation/Problem 2. Emoji Detector/Program.cs
+++ 02.Programming  Fundamentals/14.0 Exam Preparation/Exam Preparation/Problem 2. Emoji Detector/Program.cs
@@ -10,35 +10,28 @@ namespace Problem_2._Emoji_Detector
         static void Main(string[] args)
         {
             string text = Console.ReadLine();
-            long totalPoints = 0;
+            long totalPoints = 1;
             string pattern = @"(\d)";
             var match = Regex.Matches(text, pattern);
-            foreach (Match item in match)
-            {
-                if (totalPoints == 0)
-                {
-                    totalPoints = int.Parse(item.Value);
-                }
-                else
-                {
-                    totalPoints *= int.Parse(item.Value);
-                }
-            }
+            match.Select(m => m.Value)
+                .Select(int.Parse)
+                .ToList()
+                .ForEach(x => totalPoints *= x);
 
 
-            string emojiPattern = @"([::|**]{2})(?<text>[A-Z][a-z]{2,})(\1)";
+            string emojiPattern = @"(:{2}|\*{2})[A-Z][a-z]{2,}(\1)";
             var validEmojis = Regex.Matches(text, emojiPattern);
             List<string> coolEmoji = new List<string>();
             int emojisCount = validEmojis.Count;
             foreach (Match emoji in validEmojis)
             {
-                int validCount = 0;
-                string validEmoji = emoji.Groups["text"].ToString();
-                for (int i = 0; i < validEmoji.Length; i++)
-                {
-                    validCount += validEmoji[i];
-                }
-                if (validCount >= totalPoints)
+
+                long coolIndex = emoji.Value
+                    .Substring(2, emoji.Value.Length - 4)
+                    .ToString()
+                    .ToCharArray()
+                    .Sum(x => (int)x);
+                if (coolIndex >= totalPoints)
                 {
                     coolEmoji.Add(emoji.Value);
                 }

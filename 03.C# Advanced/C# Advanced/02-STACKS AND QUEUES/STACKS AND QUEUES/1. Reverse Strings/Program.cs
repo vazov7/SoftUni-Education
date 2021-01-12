0fcea119d68942065250e06258e5549465166cc5
Viktor Vazov <vazov5@yahoo.com>
2021-01-12 19:44:41 +0200

    all exercises

---
+++ 03.C# Advanced/C# Advanced/02-STACKS AND QUEUES/STACKS AND QUEUES/1. Reverse Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> words = new Stack<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                words.Push(input[i]);
+            }
+            while (words.Count > 0)
+            {
+                Console.Write(words.Pop());
+            }
+        }
+
+    }
+}
---
+++ 03.C# Advanced/C# Advanced/02-STACKS AND QUEUES/STACKS AND QUEUES/2. Stack Sum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Stack<string> expression = new Stack<string>(input.Reverse());
+            while (expression.Count > 1)
+            {
+                int leftNumber = int.Parse(expression.Pop());
+                string sign = expression.Pop();
+                int rightNumber = int.Parse(expression.Pop());
+                if (sign == "+")
+                {
+                    expression.Push((leftNumber + rightNumber).ToString());
+                }
+                else if (sign == "-")
+                {
+
+                    expression.Push((leftNumber - rightNumber).ToString());
+                }
+            }
+            Console.WriteLine(expression.Pop());
+
+        }
+    }
+}

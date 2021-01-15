add7d86e84e4935c73a409d459085cdc9f0c7bc1
Viktor Vazov <vazov5@yahoo.com>
2021-01-15 18:10:08 +0200

    add exercise

---
+++ 03.C# Advanced/C# Advanced/03-Exercise Stacks and Queues/Exercise Stacks and Queues/8. Balanced Parentheses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _8._Balanced_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            bool isbalanced = true;
+
+            //  {[()]}
+            foreach (var item in input)
+            {
+                if (item == '(' || item == '{' || item == '[')
+                {
+                    stack.Push(item);
+                }
+                else if (item == ')' || item == ']' || item == '}')
+                {
+                    if (stack.Peek() == '(' && ')' == item)
+                    {
+                        stack.Pop();
+                    }
+                    else if (stack.Peek() == '{' && '}' == item)
+                    {
+                        stack.Pop();
+                    }
+                    else if (stack.Peek() == '[' && ']' == item)
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        isbalanced = false;
+                        break;
+                    }
+                }
+            }
+            if (isbalanced)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
+

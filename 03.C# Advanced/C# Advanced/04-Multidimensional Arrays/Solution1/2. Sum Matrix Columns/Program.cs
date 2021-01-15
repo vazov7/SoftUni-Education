b25fe4612eb3f7d9dbc2043884ab6d7f141ad66b
Viktor Vazov <vazov5@yahoo.com>
2021-01-15 20:47:52 +0200

    add exercise

--- 03.C# Advanced/C# Advanced/04-Multidimensional Arrays/Solution1/2. Sum Matrix Columns/Program.cs
+++ 03.C# Advanced/C# Advanced/04-Multidimensional Arrays/Solution1/2. Sum Matrix Columns/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _2._Sum_Matrix_Columns
 {
@@ -6,7 +8,30 @@ namespace _2._Sum_Matrix_Columns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> input = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            int rows = input[0];
+            int cols = input[1];
+
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                List<int> rowData = Console.ReadLine().Split().Select(int.Parse).ToList();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowData[col];
+
+                }
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sum += matrix[row,col];
+                }
+                Console.WriteLine(sum);
+            }
         }
     }
 }
---
+++ 03.C# Advanced/C# Advanced/04-Multidimensional Arrays/Solution1/5. Square with Maximum Sum/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Square_with_Maximum_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            int rows = input[0];
+            int cols = input[1];
+
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                List<int> rowData = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowData[col];
+
+                }
+            }
+            int squareMaxSum = int.MinValue;
+            int maxSumRow = 0;
+            int maxSumCol = 0;
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    int squareSum = matrix[row, col] + matrix[row, col + 1] + matrix[row+1 , col] + matrix[row + 1, col + 1];
+                    if (squareSum > squareMaxSum)
+                    {
+                        squareMaxSum = squareSum;
+                        maxSumRow = row;
+                        maxSumCol = col;
+                    }
+                }
+            }
+            for (int row = maxSumRow; row <= maxSumRow+1 ; row++)
+            {
+                for (int col = maxSumCol; col <= maxSumCol+1; col++)
+                {
+                    Console.Write(matrix[row,col]+ " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(squareMaxSum);
+
+
+        }
+    }
+}

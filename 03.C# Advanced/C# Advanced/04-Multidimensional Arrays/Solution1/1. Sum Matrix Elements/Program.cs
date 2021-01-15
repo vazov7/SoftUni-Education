0c2de4b7f95f297356c88690876de149aacb5c75
Viktor Vazov <vazov5@yahoo.com>
2021-01-15 18:54:38 +0200

    add exercise

---
+++ 03.C# Advanced/C# Advanced/04-Multidimensional Arrays/Solution1/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            int rows = input[0];
+            int cols = input[1];
+            int sum = 0;
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                List<int> rowData = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+
+            }
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    sum += matrix[row, col];
+                }
+            }
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+        }
+    }
+}

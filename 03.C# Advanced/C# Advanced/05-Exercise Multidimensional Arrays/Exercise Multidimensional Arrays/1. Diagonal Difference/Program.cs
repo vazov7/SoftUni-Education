1538397170999381d9f3094d708a1f63b567e076
Viktor Vazov <vazov5@yahoo.com>
2021-01-18 21:07:29 +0200

    add exercise

---
+++ 03.C# Advanced/C# Advanced/05-Exercise Multidimensional Arrays/Exercise Multidimensional Arrays/1. Diagonal Difference/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+
+            for (int row = 0; row < n; row++)
+            {
+                List<int> inputRow = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = inputRow[col];
+                }
+            }
+            int count = n-1;
+            int sum = 0;
+            int secondSum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                sum += matrix[row, row];
+                secondSum += matrix[row, count];
+                count--;
+            }
+
+            Console.WriteLine(Math.Abs(sum - secondSum));
+        }
+    }
+}

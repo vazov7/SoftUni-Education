3363cc4ff6c506f91bef2dbba7e4aba3b6491e68
Viktor Vazov <vazov5@yahoo.com>
2021-01-18 22:39:29 +0200

    add exercise

---
+++ 03.C# Advanced/C# Advanced/05-Exercise Multidimensional Arrays/Exercise Multidimensional Arrays/2.  2X2 Squares in Matrix/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.__2X2_Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int inputRow = matrixSize[0];
+            int intputCol = matrixSize[1];
+
+            int[,] matrix = new int[inputRow,intputCol];
+
+            for (int row = 0; row < inputRow; row++)
+            {
+                List<char> input = Console.ReadLine().Split().Select(char.Parse).ToList(); 
+                for (int col = 0; col < intputCol; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            int count = 0;
+            for (int row = 0; row < inputRow-1; row++)
+            {
+                for (int col = 0; col < intputCol-1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] && matrix[row + 1, col] == matrix[row, col] && matrix[row, col + 1] == matrix[row + 1, col + 1])
+                    {
+                        count++;
+                    }
+
+
+                }
+            }
+           
+            
+                Console.WriteLine(count);
+            
+        }
+    }
+}

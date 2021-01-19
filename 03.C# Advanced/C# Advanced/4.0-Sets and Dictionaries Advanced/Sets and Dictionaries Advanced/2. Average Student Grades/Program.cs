c17f302218c7d62f0f8c0cacde8bd4b5dd5b0838
Viktor Vazov <vazov5@yahoo.com>
2021-01-19 19:53:56 +0200

    add exercise

---
+++ 03.C# Advanced/C# Advanced/4.0-Sets and Dictionaries Advanced/Sets and Dictionaries Advanced/2. Average Student Grades/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<decimal>> studentGrades = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                List<string> input = Console.ReadLine().Split().ToList();
+                string name = input[0];
+                decimal grade = decimal.Parse(input[1]);
+
+
+                if (!studentGrades.ContainsKey(name))
+                {
+                    studentGrades.Add(name, new List<decimal>{grade});
+                }
+                else
+                {
+                    studentGrades[name].Add(grade);
+                }
+            }
+            foreach (var key in studentGrades)
+            {
+                Console.Write($"{key.Key} -> ");
+                foreach (var student in key.Value)
+                {
+                    Console.Write($"{student:f2} ");
+                }
+                Console.Write($"avg: ({key.Value.Average()})");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 03.C# Advanced/C# Advanced/4.0-Sets and Dictionaries Advanced/Sets and Dictionaries Advanced/3. Product Shop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Product_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, decimal>> products = new Dictionary<string, Dictionary<string, decimal>>();
+            string input = Console.ReadLine();
+            while (input != "Revision")
+            {
+                List<string> splitedInput = input.Split(", ").ToList();
+                string market = splitedInput[0];
+                string product = splitedInput[0];
+                decimal price = decimal.Parse(splitedInput[3]);
+                if (!products.ContainsKey(market))
+                {
+                    products.Add(market , new Dictionary<string, decimal>());
+                }
+
+            }
+
+
+
+
+        }
+    }
+}

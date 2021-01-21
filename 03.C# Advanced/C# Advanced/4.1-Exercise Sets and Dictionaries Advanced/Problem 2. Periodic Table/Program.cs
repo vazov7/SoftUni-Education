0d1b2660cba05199856fcb1d933126136a46e7ed
Viktor Vazov <vazov5@yahoo.com>
2021-01-21 20:44:40 +0200

    add exercise

---
+++ 03.C# Advanced/C# Advanced/4.1-Exercise Sets and Dictionaries Advanced/Problem 2. Periodic Table/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_2._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> elements = new SortedSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                List<string>  input = Console.ReadLine().Split().ToList();
+                foreach (var item in input)
+                {
+                elements.Add(item);
+
+                }
+            }
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}

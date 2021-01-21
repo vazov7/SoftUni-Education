1b9915725861ce4ff76b271c75d8f67519de66ba
Viktor Vazov <vazov5@yahoo.com>
2021-01-21 20:27:14 +0200

    add exercises

---
+++ 03.C# Advanced/C# Advanced/4.1-Exercise Sets and Dictionaries Advanced/Problem 1. Sets of Elements/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_1._Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nAndm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = nAndm[0];
+            int m = nAndm[1];
+            List<int> firstGroup = new List<int>();
+            List<int> secondGroup = new List<int>();
+            List<int> totalNumbers = new List<int>();
+            int count = 0;
+
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                firstGroup.Add(number);
+            }
+            for (int i = 0; i < m; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                secondGroup.Add(number);
+            }
+            //if (firstGroup.Count > secondGroup.Count)
+            //{
+            //    count = secondGroup.Count;
+            //    for (int i = 0; i < (count); i++)
+            //    {
+            //        if (firstGroup.Contains(secondGroup[i]))
+            //        {
+            //            totalNumbers.Add(secondGroup[i]);
+            //        }
+            //    }
+            //}
+            //else
+            //{
+                for (int i = 0; i < n; i++)
+                {
+                    if (secondGroup.Contains(firstGroup[i]))
+                    {
+                        totalNumbers.Add(firstGroup[i]);
+                    }
+                }
+            //}
+          //  List<int> output = secondGroup.Where(x => firstGroup.Contains(x)).ToList();
+            
+            Console.WriteLine(string.Join(" ", totalNumbers));
+
+        }
+    }
+}
---
+++ 03.C# Advanced/C# Advanced/4.1-Exercise Sets and Dictionaries Advanced/• Unique Usernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace __Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                names.Add(input);
+            }
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

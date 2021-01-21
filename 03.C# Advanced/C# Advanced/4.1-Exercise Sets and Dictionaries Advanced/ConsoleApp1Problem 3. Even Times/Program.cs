27951e996d6709d697b65eb1adfe0833972d2674
Viktor Vazov <vazov5@yahoo.com>
2021-01-21 20:59:58 +0200

    add exercise

---
+++ 03.C# Advanced/C# Advanced/4.1-Exercise Sets and Dictionaries Advanced/ConsoleApp1Problem 3. Even Times/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1Problem_3._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                if (numbers.ContainsKey(input))
+                {
+                    numbers[input]++;
+                }
+                else
+                {
+                    numbers.Add(input,1);
+
+                }
+            }
+            foreach (var item in numbers)
+            {
+                if (item.Value % 2 == 0 && item.Value != 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+        }
+    }
+}
---
+++ 03.C# Advanced/C# Advanced/4.1-Exercise Sets and Dictionaries Advanced/Problem 4. Count Symbols/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem_4._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

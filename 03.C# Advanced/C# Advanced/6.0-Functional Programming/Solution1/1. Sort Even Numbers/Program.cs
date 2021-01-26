007a56511e1fd574857f0e73e565b49c169fa5fa
Viktor Vazov <vazov5@yahoo.com>
2021-01-26 21:32:06 +0200

    add exercise

---
+++ 03.C# Advanced/C# Advanced/6.0-Functional Programming/Solution1/1. Sort Even Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Where(n => int.Parse(n) % 2 == 0).Select(int.Parse).OrderBy(x => x).ToArray();
+            Console.WriteLine(string.Join(", ", numbers));
+        } 
+    }
+}
---
+++ 03.C# Advanced/C# Advanced/6.0-Functional Programming/Solution1/2. Sum Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

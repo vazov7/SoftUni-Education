c4fb5671b55829b8889cff765ba974599f3e4284
Viktor Vazov <vazov5@yahoo.com>
2021-01-28 19:24:04 +0200

    add

---
+++ 03.C# Advanced/C# Advanced/6.1-Exercise Functional Programming/Exercise Functional Programming/1. Action Point/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+            List<string> newnames = names.Select(name => $"{name}").ToList();
+
+            Action<List<string>> printNames = n => Console.WriteLine(string.Join(Environment.NewLine, n));
+            printNames(newnames);
+        }
+    }
+}
---
+++ 03.C# Advanced/C# Advanced/6.1-Exercise Functional Programming/Exercise Functional Programming/3. Custom Min Function/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Func<List<int>, int> minNum = num => num.Min();
+
+            int result = minNum(numbers);
+            Console.WriteLine(result);
+
+            //Func<List<int>, int> minNum =
+            //    num =>
+            //    {
+            //        int minNum = int.MaxValue;
+            //        foreach (var item in num)
+            //        {
+            //            if (item < minNum)
+            //            {
+            //                minNum = item;
+            //            }
+            //        }
+            //        return minNum;
+            //    };
+
+
+        }
+    }
+}

90d8f3df4194b12f4ef44d43ddd2481351bceae1
Viktor Vazov <vazov5@yahoo.com>
2021-01-28 19:53:36 +0200

    add ex

---
+++ 03.C# Advanced/C# Advanced/6.1-Exercise Functional Programming/Exercise Functional Programming/4. Find Evens or Odds/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int start = input[0];
+            int end = input[1];
+            string evenOrOdd = Console.ReadLine();
+
+            Func<int, int, List<int>> generateNumbers = (s, e) =>
+             {
+                 List<int> numbers = new List<int>();
+
+                 for (int i = s; i <= e; i++)
+                 {
+                     if (evenOrOdd == "even" && i % 2 == 0)
+                     {
+
+                         numbers.Add(i);
+                     }
+                     else if (evenOrOdd == "odd" && i % 2 != 0)
+                     {
+                         numbers.Add(i);
+                     }
+                 }
+                 return numbers;
+             };
+            Console.WriteLine(string.Join(" ",generateNumbers(start,end)));
+
+        }
+    }
+}

7b26b21fc7a22fed0e28083a92276dee8b7b8b70
Viktor Vazov <vazov5@yahoo.com>
2021-01-28 19:05:43 +0200

    add

---
+++ 03.C# Advanced/C# Advanced/6.1-Exercise Functional Programming/Exercise Functional Programming/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+            List<string> newnames = names.Select(name => $"Sir {name}").ToList();
+
+            Action<List<string>> printNames = n => Console.WriteLine(string.Join(Environment.NewLine,n));
+            printNames(newnames);
+        }
+      
+    }
+}

262a4fb4591c3259aace1c10b9ee28ed78ca5e60
Viktor Vazov <vazov5@yahoo.com>
2021-02-05 21:01:16 +0200

    add exercise

---
+++ 03.C# Advanced/C# Advanced/9.0-Generics/Generics/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator<T>
+    {
+       public T[] Create(int lenght, T item)
+        {
+            T[] array = new T[lenght];
+            array[0] = item;
+            return array;
+        }
+    
+        
+    }
+}
---
+++ 03.C# Advanced/C# Advanced/9.0-Generics/Generics/GenericArrayCreator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ArrayCreator<string> stringArray = new ArrayCreator<string>();
+            string[] strings = stringArray.Create(5, "Pesho");
+            for (int i = 0; i < strings.Length; i++)
+            {
+                Console.WriteLine(strings[i]);
+            }
+        }
+    }
+}

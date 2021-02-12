bcda15f1650fddd1da3334a7330d67a2cd57f881
Viktor Vazov <vazov5@yahoo.com>
2021-02-12 21:47:19 +0200

    add exam prep

---
+++ 03.C# Advanced/C# Advanced/11.0-Exam Preparatin/Exam Preparation/Bee/Program.cs
@@ -0,0 +1,236 @@
+﻿using System;
+
+namespace Bee
+{
+    class Program
+    {
+        
+       static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            int[] indexOfBee = new int[2];
+            int[] bonusIndex = new int[2];
+            for (int col = 0; col < n; col++)
+            {
+                string input = Console.ReadLine();
+                for (int row = 0; row < n; row++)
+                {
+                    matrix[col, row] = input[row];
+                    if (input[row] == 'B')
+                    {
+                        indexOfBee[0] = col;
+                        indexOfBee[1] = row;
+                    }
+                    if (input[row] == 'O')
+                    {
+                        bonusIndex[0] = col;
+                        bonusIndex[1] = row;
+                    }
+                }
+            }
+            int pollinateFlowers = 0;
+            string command = string.Empty;
+            while ((command = Console.ReadLine().ToLower()) != "end")
+            {
+                if (command == "up")
+                {
+                    indexOfBee[0] = indexOfBee[0] - 1;
+                    if (IsInRange(n, indexOfBee[0], indexOfBee[1]))
+                    {
+                        if (matrix[indexOfBee[0], indexOfBee[1]] == 'O')
+                        {
+                            indexOfBee[0] = indexOfBee[0] - 1;
+                            if (!IsInRange(n, indexOfBee[0], indexOfBee[1]))
+                                break;
+
+                            matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                            matrix[indexOfBee[0] + 1, indexOfBee[1]] = '.';
+                            if (matrix[indexOfBee[0], indexOfBee[1]] == 'f')
+                            {
+
+                                pollinateFlowers++;
+                                matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                                matrix[indexOfBee[0] + 1, indexOfBee[1]] = '.';
+                            }
+                            else
+                            {
+                                matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                                matrix[indexOfBee[0] + 1, indexOfBee[1]] = '.';
+                            }
+
+                        }
+                        else if (matrix[indexOfBee[0], indexOfBee[1]] == 'f')
+                        {
+
+                            pollinateFlowers++;
+                            matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                            matrix[indexOfBee[0] + 1, indexOfBee[1]] = '.';
+                        }
+                        else
+                        {
+                            matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                            matrix[indexOfBee[0] + 1, indexOfBee[1]] = '.';
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+                else if (command == "down")
+                {
+                    indexOfBee[0] = indexOfBee[0] + 1;
+                    if (IsInRange(n, indexOfBee[0], indexOfBee[1]))
+                    {
+                        if (matrix[indexOfBee[0], indexOfBee[1]] == 'O')
+                        {
+                            
+                            indexOfBee[0] = indexOfBee[0] + 1;
+                            if (!IsInRange(n, indexOfBee[0], indexOfBee[1]))
+                                break;
+                            matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                            matrix[indexOfBee[0] - 1, indexOfBee[1]] = '.';
+                            if (matrix[indexOfBee[0], indexOfBee[1]] == 'f')
+                            {
+
+                                pollinateFlowers++;
+                                matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                                matrix[indexOfBee[0] - 1, indexOfBee[1]] = '.';
+                            }
+                            else
+                            {
+                                matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                                matrix[indexOfBee[0] + 1, indexOfBee[1]] = '.';
+                            }
+
+                        }
+                        else if (matrix[indexOfBee[0], indexOfBee[1]] == 'f')
+                        {
+
+                            pollinateFlowers++;
+                            matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                            matrix[indexOfBee[0] - 1, indexOfBee[1]] = '.';
+                        }
+                        else
+                        {
+                            matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                            matrix[indexOfBee[0] - 1, indexOfBee[1]] = '.';
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+                else if (command == "left")
+                {
+                    indexOfBee[1] = indexOfBee[1] - 1;
+                    if (IsInRange(n, indexOfBee[0], indexOfBee[1]))
+                    {
+                        if (matrix[indexOfBee[0], indexOfBee[1]] == 'O')
+                        {
+                          
+                            indexOfBee[1] = indexOfBee[1] - 1;
+                            if (!IsInRange(n, indexOfBee[0], indexOfBee[1]))
+                                break;
+
+                            matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                            matrix[indexOfBee[0], indexOfBee[1] + 1] = '.';
+                            if (matrix[indexOfBee[0], indexOfBee[1]] == 'f')
+                            {
+
+                                pollinateFlowers++;
+                                matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                                matrix[indexOfBee[0], indexOfBee[1] + 1] = '.';
+                            }
+                            else
+                            {
+                                matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                                matrix[indexOfBee[0], indexOfBee[1] + 1] = '.';
+                            }
+
+                        }
+                        else if (matrix[indexOfBee[0], indexOfBee[1]] == 'f')
+                        {
+
+                            pollinateFlowers++;
+                            matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                            matrix[indexOfBee[0], indexOfBee[1] + 1] = '.';
+                        }
+                        else
+                        {
+                            matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                            matrix[indexOfBee[0], indexOfBee[1] + 1] = '.';
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else if (command == "right")
+                {
+                    indexOfBee[1] = indexOfBee[1] + 1;
+                    if (IsInRange(n, indexOfBee[0], indexOfBee[1]))
+                    {
+
+                        if (matrix[indexOfBee[0], indexOfBee[1]] == 'O')
+                        {
+                            matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                            matrix[indexOfBee[0], indexOfBee[1] - 1] = '.';
+                            indexOfBee[1] = indexOfBee[1] + 1;
+                            if (!IsInRange(n, indexOfBee[0], indexOfBee[1]))
+                                break;
+
+                            matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                            matrix[indexOfBee[0], indexOfBee[1] - 1] = '.';
+                            if (matrix[indexOfBee[0], indexOfBee[1]] == 'f')
+                            {
+
+                                pollinateFlowers++;
+                                matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                                matrix[indexOfBee[0], indexOfBee[1] - 1] = '.';
+                            }
+                            else
+                            {
+                                matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                                matrix[indexOfBee[0], indexOfBee[1] - 1] = '.';
+                            }
+
+                        }
+                        else if (matrix[indexOfBee[0], indexOfBee[1]] == 'f')
+                        {
+
+                            pollinateFlowers++;
+                            matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                            matrix[indexOfBee[0], indexOfBee[1] - 1] = '.';
+                        }
+                        else
+                        {
+                            matrix[indexOfBee[0], indexOfBee[1]] = 'B';
+                            matrix[indexOfBee[0], indexOfBee[1] - 1] = '.';
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+                    
+            }
+
+        }
+        static bool IsInRange(int n, int col, int row)
+        {
+            if ((col >= 0 && col < n) && (row >= 0 && row < n))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ 03.C# Advanced/C# Advanced/11.0-Exam Preparatin/Exam Preparation/Flower Wreaths/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Flower_Wreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> lilies = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+
+            Queue<int> roses = new Queue<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            int wreath = 0;
+            int flowers = 0;
+            while (lilies.Count > 0 && roses.Count > 0)
+            {
+                int lilia = lilies.Pop();
+                int rose = roses.Dequeue();
+                int sum = lilia + rose;
+                if (sum == 15)
+                {
+                    wreath++;
+                }
+                else if (sum < 15)
+                {
+                    flowers += sum;
+                }
+                else if (sum > 15)
+                {
+                    while (sum > 15)
+                    {
+                        sum -= 2;
+                        if (sum == 15)
+                        {
+                            wreath++;
+                        }
+                        else if (sum < 15)
+                        {
+                            flowers += sum;
+                        }
+                    }
+                }
+            }
+            wreath += flowers / 15;
+
+            if (wreath >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreath} wreaths!" );
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreath} wreaths more!");
+            }
+        }
+    }
+}

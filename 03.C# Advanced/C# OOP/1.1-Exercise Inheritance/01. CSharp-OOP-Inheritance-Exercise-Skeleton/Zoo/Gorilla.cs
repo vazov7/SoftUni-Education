eb5a9fa6d1a1b09e35d44e208a5ced9bcaa10634
Viktor Vazov <vazov5@yahoo.com>
2021-02-25 19:59:58 +0200

    ad

---
+++ 03.C# Advanced/C# OOP/1.1-Exercise Inheritance/01. CSharp-OOP-Inheritance-Exercise-Skeleton/Zoo/Gorilla.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Gorilla : Mammal
+    {
+
+        public Gorilla(string name)
+            :base(name)
+        {
+
+        }
+        public string Name { get; set; }
+    }
+}
---
+++ 03.C# Advanced/C# OOP/1.1-Exercise Inheritance/01. CSharp-OOP-Inheritance-Exercise-Skeleton/Zoo/Lizard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Lizard : Reptile
+    {
+        public Lizard(string name)
+            :base(name)
+            
+        {
+
+        }
+
+        public string Name { get; set; }
+
+    }
+}

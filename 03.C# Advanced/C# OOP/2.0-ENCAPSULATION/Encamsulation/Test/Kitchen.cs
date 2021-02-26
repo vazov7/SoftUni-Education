b4ed00a3692b6ee2dc3b08f986ce1ef2d2b109e7
Viktor Vazov <vazov5@yahoo.com>
2021-02-26 20:11:04 +0200

    add encapsulation

---
+++ 03.C# Advanced/C# OOP/2.0-ENCAPSULATION/Encamsulation/Test/Kitchen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class Kitchen
+    {
+        private int meat;
+        private int garlic; 
+        public Kitchen(int meat,int garlic)
+        {
+            this.meat = meat;
+            this.garlic = garlic;
+        }
+        public void CookMeatball()
+        {
+
+        }
+    }
+}

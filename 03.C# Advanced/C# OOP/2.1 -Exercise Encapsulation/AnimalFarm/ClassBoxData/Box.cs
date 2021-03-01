df4ea4037ed74babf0fd9eb2f1e460f8e9c1a07d
Viktor Vazov <vazov5@yahoo.com>
2021-03-01 18:51:01 +0200

    add

---
+++ 03.C# Advanced/C# OOP/2.1 -Exercise Encapsulation/AnimalFarm/ClassBoxData/Box.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        public Box(double a, double b, double c)
+        {
+            this.Lenght = a;
+            this.Width = b;
+            this.Height = c;
+        }
+        private double lenght;
+        private double width;
+        private double height;
+
+        public double Lenght
+        {
+            get => this.lenght;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Lenght cannot be zero or negative.");
+                }
+                else
+                {
+                    this.lenght = value;
+                }
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                else
+                {
+                    this.width = value;
+                }
+            }
+        }
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                else
+                {
+                    this.height = value;
+                }
+            }
+        }
+        public double CalculateValue()
+        {
+            return this.Width * this.Lenght * this.Height;
+        }
+        public double CalculateLateralSurfaceArea()
+        {
+            return 2 * Lenght * Height + 2 * Width * Height;
+        }
+        public double CalculateSurfaceArea()
+        {
+            return 2 * Lenght * Width + 2 * Lenght * Height + 2 * Width * Height;
+        }
+    }
+}
---
+++ 03.C# Advanced/C# OOP/2.1 -Exercise Encapsulation/AnimalFarm/ClassBoxData/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double l = double.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            try
+            {
+                var box = new Box(l, w, h);
+                Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {box.CalculateValue():F2}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}

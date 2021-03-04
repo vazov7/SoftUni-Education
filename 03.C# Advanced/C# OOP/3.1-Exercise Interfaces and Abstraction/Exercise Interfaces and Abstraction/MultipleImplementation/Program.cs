752645c945ae37326161d0f8c0fb7306cfed555e
Viktor Vazov <vazov5@yahoo.com>
2021-03-04 18:53:37 +0200

    add

---
+++ 03.C# Advanced/C# OOP/3.1-Exercise Interfaces and Abstraction/Exercise Interfaces and Abstraction/MultipleImplementation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultipleImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

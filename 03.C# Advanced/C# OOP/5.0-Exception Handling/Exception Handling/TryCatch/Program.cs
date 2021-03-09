253ebf1f428924aa8ff08c356d93ea7f179f1b67
Viktor Vazov <vazov5@yahoo.com>
2021-03-09 21:02:31 +0200

    add

---
+++ 03.C# Advanced/C# OOP/5.0-Exception Handling/Exception Handling/TryCatch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            try
+            {
+                Console.WriteLine("kolko si durt");
+                age = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("tolkova LI NE MOJESH DA NAPISHEHS ENDO CHISLO");
+
+                try
+                {
+                    age = int.Parse(Console.ReadLine());
+
+                }
+                catch (Exception exsecond)
+                {
+                    Console.WriteLine(exsecond.Message);
+                    Console.WriteLine(exsecond.GetType());
+                    Console.WriteLine("chslo napishi be chpovek cghislo");
+                    age = int.Parse(Console.ReadLine());
+                }
+               
+
+
+            }
+            Console.WriteLine($"{age} abe durt si");
+        }
+    }
+}

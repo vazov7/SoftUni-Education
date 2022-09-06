8918b757d6590cb905cdf8f040a8199a08fe097b
Viktor Vazov <vazov5@yahoo.com>
2022-09-06 22:49:54 +0300

    add some exercises

---
+++ 02.Programming  Fundamentals/11.0 - Associative Arrays/Solution1/5.OddOcurnes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _5.OddOcurnes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //read list of strings(words) and make it to lower case
+            List<string> input = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(n => n.ToLower())
+                .ToList();
+
+            //count how much words repeats
+            Dictionary<string,int> output = new Dictionary<string,int>();
+
+            foreach (var item in input)
+            {
+                if (output.ContainsKey(item))
+                {
+                    output[item]++;
+                }
+                else
+                {
+                    output.Add(item, 1);
+                }
+            }
+            //check if repeats is odd print it or ignore it
+            foreach (var item in output)
+            {
+                if (item.Value % 2 == 1)
+                {
+                    Console.Write($" {item.Key}");
+                }
+            }
+        }
+    }
+}

14c3075cc1362e7104acbe4ad9fb7710a0f8475f
Viktor Vazov <vazov5@yahoo.com>
2022-09-23 21:11:08 +0300

    exercise for regex

---
+++ 02.Programming  Fundamentals/13.0-Regular Expressions/Regular Exp/MatchDates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = ("\\b(?<day>\\d{2})([-.\\/])(?<month>[A-Z][a-z]{2})\\1(?<year>\\d{4})\\b");
+            string input = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(input, regex);
+
+
+            foreach (Match item in matches)
+            {
+
+                //Console.WriteLine(item.Groups["day"].Value);
+                Console.WriteLine($"Day: {item.Groups["day"].Value}, Month: {item.Groups["month"].Value}, Year: {item.Groups["year"].Value}");
+            }
+
+        }
+    }
+}
---
+++ 02.Programming  Fundamentals/13.0-Regular Expressions/Regular Exp/MatchPhoneNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = ("\\+359([- ])2\\1(\\d{3})\\1(\\d{4})\\b");
+            string input = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(input, regex);
+
+            Console.WriteLine(string.Join(", ",matches.Cast<Match>()
+                .Select(a=> a.Value.Trim())
+                .ToList()));
+            
+        }
+    }
+}

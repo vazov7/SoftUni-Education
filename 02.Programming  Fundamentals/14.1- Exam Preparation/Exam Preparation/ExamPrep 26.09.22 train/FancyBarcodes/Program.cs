b1d71715dac694f99bd5221f9837772801ad6a79
Viktor Vazov <vazov5@yahoo.com>
2022-09-26 23:12:55 +0300

    add some exercise

---
+++ 02.Programming  Fundamentals/14.1- Exam Preparation/Exam Preparation/ExamPrep 26.09.22 train/FancyBarcodes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace FancyBarcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(@#+)(?<body>[A-Z][A-Za-z\d]{4,}[A-Z])(@#+)";
+            Regex regex = new Regex(pattern);
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string productGroup = "00";
+                string input = Console.ReadLine();
+                Match match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    string body = match.Groups["body"].ToString();
+                    //int nums = match.Groups["body"].ToString().Select(Char.)
+                    string rawGroup = string.Empty;
+                    for (int v = 0; v < body.Length; v++)
+                    {
+                        if (Char.IsDigit(body[v]))
+                            rawGroup += body[v];
+                    }
+                    if (rawGroup.Length > 0)
+                        productGroup = rawGroup;
+
+                    Console.WriteLine($"Product group: {productGroup}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+        }
+        
+    }
+}
---
+++ 02.Programming  Fundamentals/14.1- Exam Preparation/Exam Preparation/ExamPrep 26.09.22 train/PasswordReset/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PasswordReset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<char> rawPassword = new List<char>();
+            string cmd = string.Empty;
+            while ((cmd = Console.ReadLine()) != "Done")
+            {
+                List<string> command = cmd
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                if (command[0] == "TakeOdd")
+                {
+
+
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        if (i % 2 == 1)
+                        {
+                            rawPassword.Add(input[i]);
+
+                        }
+                    }
+                    string password = string.Join("", rawPassword);
+                    Console.WriteLine(password);
+                }
+                else if (command[0] == "Cut")
+                {
+                    rawPassword.RemoveRange(int.Parse(command[1]), int.Parse(command[2]));
+                    string password = string.Join("", rawPassword);
+                    Console.WriteLine(password);
+                }
+                else if (command[0] == "Substitute")
+                {
+                    string password = string.Join("",rawPassword);
+                    if (password.Contains(command[1]))
+                    {
+                        string newstr = password.Replace(command[1], command[2]);
+                        password = newstr;
+                        rawPassword.Clear();
+                        for (int i = 0; i < password.Length; i++)
+                        {
+                            rawPassword.Add((char)password[i]);
+                        }
+                        Console.WriteLine(password);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing to replace!");
+                    }
+                }
+            }
+            string password = string.Join("", rawPassword);
+            ;
+            Console.WriteLine($"Your password is: {password}");
+
+
+
+        }
+    }
+}

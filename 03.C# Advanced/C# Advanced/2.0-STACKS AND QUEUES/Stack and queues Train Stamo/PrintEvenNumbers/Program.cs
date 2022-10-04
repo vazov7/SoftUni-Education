6acb8342006fb55f4f2396bf1fdf569e7ed135e6
Viktor Vazov <vazov5@yahoo.com>
2022-10-05 00:13:08 +0300

    add some exercises

---
+++ 03.C# Advanced/C# Advanced/2.0-STACKS AND QUEUES/Stack and queues Train Stamo/PrintEvenNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            int[] inputArr = n.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> evenNums = new Queue<int>();
+            foreach (var item in inputArr)
+            {
+                if (item % 2 == 0)
+                {
+                    evenNums.Enqueue(item);
+                }
+            }
+            StringBuilder output = new StringBuilder();
+            output.Append(evenNums.Dequeue());
+
+            foreach (var car in evenNums)
+            {
+                output.Append($", {car}");
+
+            }
+            Console.WriteLine(output.ToString());
+        }
+    }
+}

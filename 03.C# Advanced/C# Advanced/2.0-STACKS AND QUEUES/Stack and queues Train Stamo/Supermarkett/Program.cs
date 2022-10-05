787d362bc1154cda2a6ea318bcec4a713124e05b
Viktor Vazov <vazov5@yahoo.com>
2022-10-05 22:34:00 +0300

    add some exercise on stacks and queue

---
+++ 03.C# Advanced/C# Advanced/2.0-STACKS AND QUEUES/Stack and queues Train Stamo/Supermarkett/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Supermarkett
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Queue<string> queue = new Queue<string>();
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input == "Paid")
+                {
+                    int n = queue.Count;
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+            }
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
---
+++ 03.C# Advanced/C# Advanced/2.0-STACKS AND QUEUES/Stack and queues Train Stamo/TrafficJam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafficJam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int trafficCars = int.Parse(Console.ReadLine());
+            string input;
+            Queue<string> queue = new Queue<string>();
+            int count = 0;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input == "green")
+                {
+                    
+                    for (int i = 0; i < trafficCars; i++)
+                    {
+                        if (queue.Count ==0 )
+                        {
+                            break;
+                        }
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        count++;
+                    }
+
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+            }
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}

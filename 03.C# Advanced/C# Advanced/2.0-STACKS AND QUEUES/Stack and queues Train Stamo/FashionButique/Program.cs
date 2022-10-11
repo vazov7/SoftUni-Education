1d93d36915e5a083ff66bb74ba227bf5ccb7c359
Viktor Vazov <vazov5@yahoo.com>
2022-10-12 00:37:54 +0300

    add exercises

---
+++ 03.C# Advanced/C# Advanced/2.0-STACKS AND QUEUES/Stack and queues Train Stamo/FashionButique/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FashionButique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+            int sum = 0;
+            int neededRacks = 1;
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+            
+            while (stack.Count > 0)
+            {
+                sum += stack.Peek();
+                if (sum == rackCapacity && stack.Count >0)
+                {
+                    neededRacks++;
+                    sum = 0;
+                }
+                else if (sum > rackCapacity)
+                {
+                    //ако суматата остава по голяма от мястото на рака може да гръмне
+                    neededRacks++;
+                    sum = 0;
+                }
+                else if (stack.Count == 1 && sum < rackCapacity)
+                {
+                    neededRacks++;
+                }
+                stack.Pop();
+            }
+            Console.WriteLine(neededRacks);
+
+        }
+    }
+}
